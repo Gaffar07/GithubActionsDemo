1b3a077aa85a6ed07a8d69c81486387aed0f7a58
gaffar <gaffarshaikh07@gmail.com>
2025-11-09 19:31:58 +0530

    last commit

--- test-output-timelines/report.js
+++ test-output-timelines/report.js
@@ -1,4 +1,4 @@
 $(document).ready(function() {
-CucumberHTML.timelineItems.pushArray([{"id":"f1f30eb9-25a7-4b3e-a32a-7591a34dd6e2","feature":"Hero Feature file","scenario":"as  a user i\tcan verify food menu ovetr hero module","start":1762687934525,"group":22,"content":"","tags":"@hoome,@hero,@regression,@noorder,@hero1,@tc01,@medium,","end":1762687941735,"className":"passed"}]);
+CucumberHTML.timelineItems.pushArray([{"id":"0c3a4109-8a47-4cfe-b247-53cc5bdb5fee","feature":"Hero Feature file","scenario":"as  a user i\tcan verify food menu ovetr hero module","start":1762696576241,"group":22,"content":"","tags":"@hoome,@hero,@regression,@noorder,@hero1,@tc01,@medium,","end":1762696582910,"className":"passed"}]);
 CucumberHTML.timelineGroups.pushArray([{"id":22,"content":"Thread[TestNG-PoolService-0,5,main]"}]);
 });
