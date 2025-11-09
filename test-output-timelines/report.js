69f523d3f5e7d66571e99fedd48ed2400a27685a
gaffar <gaffarshaikh07@gmail.com>
2025-11-09 20:07:33 +0530

    headless code added

--- test-output-timelines/report.js
+++ test-output-timelines/report.js
@@ -1,4 +1,4 @@
 $(document).ready(function() {
-CucumberHTML.timelineItems.pushArray([{"id":"0c3a4109-8a47-4cfe-b247-53cc5bdb5fee","feature":"Hero Feature file","scenario":"as  a user i\tcan verify food menu ovetr hero module","start":1762696576241,"group":22,"content":"","tags":"@hoome,@hero,@regression,@noorder,@hero1,@tc01,@medium,","end":1762696582910,"className":"passed"}]);
+CucumberHTML.timelineItems.pushArray([{"id":"ff688d2f-13a5-4ddb-9aeb-f7ce4376d614","feature":"Hero Feature file","scenario":"as  a user i\tcan verify food menu ovetr hero module","start":1762699018565,"group":22,"content":"","tags":"@hoome,@hero,@regression,@noorder,@hero1,@tc01,@medium,","end":1762699025034,"className":"passed"}]);
 CucumberHTML.timelineGroups.pushArray([{"id":22,"content":"Thread[TestNG-PoolService-0,5,main]"}]);
 });
